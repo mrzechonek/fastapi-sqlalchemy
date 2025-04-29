3345182c8d23ca078391bb646cec8f615556ea58
Michał Lowas-Rzechonek <michal@rzechonek.net>
2025-04-29 09:29:05 +0200

    Add async session middleware

--- fastapi_sqlalchemy/__init__.py
+++ fastapi_sqlalchemy/__init__.py
@@ -1,5 +1,6 @@
 from fastapi_sqlalchemy.middleware import DBSessionMiddleware, db
+from fastapi_sqlalchemy.async_middleware import AsyncDBSessionMiddleware, async_db
 
-__all__ = ["db", "DBSessionMiddleware"]
+__all__ = ["db", "async_db", "DBSessionMiddleware", "AsyncDBSessionMiddleware"]
 
-__version__ = "0.2.1"
+__version__ = "0.2.1.post1"
